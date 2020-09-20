58d2c1354b155d396046532879ff34ac21547b1a
Kasra Alexander Sohrab <ksohrab3@gatech.edu>
2020-09-20 13:11:34 -0400

    Fixed exercise 1.0

--- exercise_1_0/exercise_1_0.cpp
+++ exercise_1_0/exercise_1_0.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main() {
     // TODO: Make this program print "Hello, world!" to the console.
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
 }
