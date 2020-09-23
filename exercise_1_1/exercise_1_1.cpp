5bde0824c0ff45b511e026d727b8a477a753573f
Kasra Alexander Sohrab <ksohrab3@gatech.edu>
2020-09-22 21:10:21 -0400

    Fixed exercise 1.1

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -51,6 +51,9 @@ int main() {
 				else {
 					std::cout << "Sorry, I didn't get that. Try again." << std::endl;
 				}
+				for (int i = 0; i < 4; i++) {
+							 std::cout << "wheel at index " << i << " has speed: " << wheel_speeds[i] << std::endl;
+			 	}
 				}
         //
   }
